5bbac9588848d568830819e3412eeef8a1c06381
cong86035 <cong86035@gmail.com>
2025-12-16 16:41:57 +0700

    Resolve conflict

---
+++ SupermaketmanagerProduct/src/test/java/fu/hsf302/supermaketmanagerproduct/SupermaketmanagerProductApplicationTests.java
@@ -0,0 +1,13 @@
+package fu.hsf302.supermaketmanagerproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermaketmanagerProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
