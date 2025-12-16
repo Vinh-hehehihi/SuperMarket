bd725e3dfbd3ff1d1552f436fb36a78f1f417eb4
Vinh-hehehihi <quangvinh23072004@gmail.com>
2025-12-16 22:09:37 +0700

    Delete SupermaketmanagerProduct directory

--- SupermaketmanagerProduct/src/main/java/fu/hsf302/supermaketmanagerproduct/repository/StallRepository.java
+++
@@ -1,7 +0,0 @@
-package fu.hsf302.supermaketmanagerproduct.repository;
-import fu.hsf302.supermaketmanagerproduct.entity.Stall;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface StallRepository extends JpaRepository<Stall, Integer> {
-}
--- SupermaketmanagerProduct/src/main/java/fu/hsf302/supermaketmanagerproduct/repository/SupplierRepository.java
+++
@@ -1,9 +0,0 @@
-package fu.hsf302.supermaketmanagerproduct.repository;
-
-import fu.hsf302.supermaketmanagerproduct.entity.Supplier;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
-}
